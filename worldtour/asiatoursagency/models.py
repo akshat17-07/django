8a753ea519d004c9b45d6ef2d1a8b3b258abf2c7
Akshat Harlalka <akshatharlalka.ah@icloud.com>
2024-11-25 05:54:48 -0500

    created first page with data

--- worldtour/asiatoursagency/models.py
+++ worldtour/asiatoursagency/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Tour(models.Model):
+    origin_country = models.CharField(max_length=64)
+    destination_country = models.CharField(max_length=64)
+    number_of_nights = models.IntegerField()
+    price = models.IntegerField()
+
+    def __str__(self):
+        return (f"""ID:{self.id}: 
+                From {self.origin_country} To {self.destination_country},
+                For {self.number_of_nights} nights,
+                price = {self.price}""")
